7c6761ec1074d1c5031dbddc36cc3e7affafb158
cristinahh <123323452+cristinahh@users.noreply.github.com>
2024-06-11 22:06:57 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')  #when this path is accessed, show whatever is after return
+def hello_world():
+  return "Hello, Cristy"
+
+
+print(__name__)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
