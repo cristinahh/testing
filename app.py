cfe5143e3d15af41289dc379fa4a9b9e05c27d59
cristinahh <123323452+cristinahh@users.noreply.github.com>
2024-07-19 20:40:58 +0000

    testing today

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')  #when this path is accessed, show whatever is after return
 def hello_world():
-  return "Hello, Cristy"
-
+  return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True) #starts the app on hose 0.0.0.0
 
 #deleted comment
