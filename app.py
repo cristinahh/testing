cb25e8c1675ec3edefa98d1b71a7a074472dac46
cristinahh <123323452+cristinahh@users.noreply.github.com>
2024-08-06 21:22:31 +0000

    added images and buttons
    added images and buttons.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-
-
 @app.route('/')  #when this path is accessed, show whatever is after return
+
 def hello_world():
   return render_template('home.html')
 
+
 print(__name__)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True) #starts the app on hose 0.0.0.0
+  app.run(host='0.0.0.0', debug=True)  #starts the app on hose 0.0.0.0
 
 #deleted comment
