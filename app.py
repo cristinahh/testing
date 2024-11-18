3172d4791aaa18ca300d840ddc80029298fecc16
Cristina Hernandez <123323452+cristinahh@users.noreply.github.com>
2024-11-18 05:23:19 +0000

    minor

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, send_from_directory
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+web3forms_access_key = os.getenv('WEB3FORMS_ACCESS_KEY')
+
+
+@app.route('/web3forms_api_key')
+def home():
+  return f"Your Web3Forms API Key is: {web3forms_access_key}"
 
 
 @app.route('/')  #when this path is accessed, show whatever is after return
 def hello_world():
   return render_template('home.html')
+
 
 @app.route(
     '/aboutme')  #when this path is accessed, show whatever is after return
@@ -31,6 +42,9 @@ def contact():
   return render_template('contact.html')
 
 
+print(
+    web3forms_access_key
+)  # This should print the value of the access key if it's loaded correctly.
 print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)  #starts the app on hose 0.0.0.0
